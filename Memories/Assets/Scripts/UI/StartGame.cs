5791444b92bafe9a91f7a50fa081b9b19ad18951
ilyadolzhenko <49413402+ilyadolzhenko@users.noreply.github.com>
2019-05-04 18:23:15 +0300

    Hint

---
+++ Memories/Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject hint;
+
+    void Start() 
+    {
+        if (hint.activeInHierarchy)
+            Time.timeScale=0;
+        else Time.timeScale=1;
+    }
+
+    void Update()
+    {
+        if (hint.activeInHierarchy && Input.anyKeyDown)
+        {
+            hint.SetActive(false);
+            Time.timeScale=1;
+            Destroy(this);
+        }
+    }
+}
