860dce42737d52f0edf22730be20a239045196e5
ilyadolzhenko <49413402+ilyadolzhenko@users.noreply.github.com>
2019-05-14 17:46:44 +0300

    Witcher animation+script

--- Memories/Assets/Scripts/Enemy/RangeEnemy.cs
+++ Memories/Assets/Scripts/Enemy/RangeEnemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RangeEnemy : EnemyBase
 {
+    public float stopDistance;
+    public float retreatDistance;
+
     void Update()
     {
         Following();
@@ -13,12 +16,20 @@ public class RangeEnemy : EnemyBase
 
     protected override void Following()
     {
-
+        if (Vector2.Distance(transform.position, target.transform.position) > stopDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        } 
+        else if (Vector2.Distance(transform.position, target.transform.position) < stopDistance && 
+                 Vector2.Distance(transform.position, target.transform.position) > retreatDistance) {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, target.transform.position) < retreatDistance) {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, -speed * Time.deltaTime);
+        }
     }
 
     protected override void Attack()
     {
 
     }
-    //Переопределить дальнюю атаку
 }
