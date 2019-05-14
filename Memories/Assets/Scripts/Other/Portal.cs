5778f01951cf308777007e7b155243ab98daa4c4
ilyadolzhenko <49413402+ilyadolzhenko@users.noreply.github.com>
2019-05-14 21:52:58 +0300

    Fix portal

--- Memories/Assets/Scripts/Other/Portal.cs
+++ Memories/Assets/Scripts/Other/Portal.cs
@@ -5,26 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    private bool enter=false;
     private GameObject m;
-
-    private void Update() 
-    {
-        if (enter && Input.GetKeyDown(KeyCode.E)) 
-        {
-            m.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        m = other.gameObject;
-        enter = true;
-    }
-
-    private void OnTriggerExit2D(Collider2D other) 
-    {
-        enter = false;
+        if (other.CompareTag("Player"))
+        {
+            m = other.gameObject;
+            m.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
