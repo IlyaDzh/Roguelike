0da4b4a8aae33b0ab2ed0a7233df356fd78a4591
ilyadolzhenko <49413402+ilyadolzhenko@users.noreply.github.com>
2019-05-22 13:18:48 +0300

    Attack archer

--- Memories/Assets/Scripts/Character/Archer.cs
+++ Memories/Assets/Scripts/Character/Archer.cs
@@ -6,6 +6,8 @@ public class Archer : PlayerBase
 {
     private GameObject projectile;
     public float projectileForce;
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
 
     void Update ()
 	{
@@ -16,16 +18,22 @@ public class Archer : PlayerBase
 
     protected override void Attack()
     {
-        if (Input.GetMouseButtonDown(0) && Time.timeScale == 1)
-        { 
-            if (!GameObject.FindGameObjectWithTag("GOManager")) return;
-            projectile = GameObject.FindGameObjectWithTag("GOManager").GetComponent<GOManager>().bullet[3];
-            Vector2 mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 myPos = transform.position;
-            Vector2 direction = (mousePos - myPos).normalized;
-            GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
-            spell.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
-            spell.GetComponent<Projectile>().damage = Random.Range(minDamage, maxDamage);
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetMouseButton(0) && Time.timeScale == 1)
+            { 
+                if (!GameObject.FindGameObjectWithTag("GOManager")) return;
+                projectile = GameObject.FindGameObjectWithTag("GOManager").GetComponent<GOManager>().bullet[3];
+                Vector2 mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 myPos = transform.position;
+                Vector2 direction = (mousePos - myPos).normalized;
+                GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
+                spell.GetComponent<Rigidbody2D>().velocity = direction * projectileForce;
+                spell.GetComponent<Projectile>().damage = Random.Range(minDamage, maxDamage);
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        } else {
+            timeBtwAttack -= Time.deltaTime;
         }
     }
 
