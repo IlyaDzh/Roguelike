b0b7aed29129a7ad82e64ae827964a51309b6dd6
ilyadolzhenko <49413402+ilyadolzhenko@users.noreply.github.com>
2019-05-23 15:35:56 +0300

    Music

--- Memories/Assets/Scripts/Music/AudioManager.cs
+++ Memories/Assets/Scripts/Music/AudioManager.cs
@@ -23,6 +23,7 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.addMixer;
         }
     }
 
